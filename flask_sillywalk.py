f89c5daa8e5e88f3b7caf32c257be5262fd81d17
Luis Carvalho <mail@lmcarvalho.com>
2014-07-23 20:38:31 +0100

    Prepare for both Python 2 and 3

--- flask_sillywalk.py
+++ flask_sillywalk.py
@@ -1,7 +1,12 @@
 import inspect
 import json
 from collections import defaultdict
-from urllib.parse import urlparse
+
+import sys
+if sys.version_info < (3, 0):
+  from urlparse import urlparse
+else:
+  from urllib.parse import urlparse
 
 try:
     from flask import _app_ctx_stack as stack
